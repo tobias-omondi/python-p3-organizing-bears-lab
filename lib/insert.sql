743a089cd52b89d6ceaf0f0c7eaade59a10a7819
tobias <tobias.omondi@student.moringaschool.com>
2023-09-26 13:05:27 +0300

    updated

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+--  Mr. Chocolate
+INSERT INTO bears (name, sex) VALUES ('Mr. Chocolate', 'M');
+
+--  Rowdy
+INSERT INTO bears (name, sex) VALUES ('Rowdy', 'M');
+
+--  Tabitha
+INSERT INTO bears (name, sex) VALUES ('Tabitha', 'F');
+
+--  Sergeant Brown
+INSERT INTO bears (name, sex) VALUES ('Sergeant Brown', 'M');
+
+--  Melissa
+INSERT INTO bears (name, sex) VALUES ('Melissa', 'F');
+
+--  Grinch
+INSERT INTO bears (name, sex) VALUES ('Grinch', 'M');
+
+--  Wendy
+INSERT INTO bears (name, sex) VALUES ('Wendy', 'F');
+
+--  unnamed
+INSERT INTO bears (name, sex) VALUES (NULL, NULL);
--- lib/test_setup.py
+++ lib/test_setup.py
@@ -1,2 +1,3 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
